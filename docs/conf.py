8ae0d42d4135fe05b393e92d550e42e81668df77
Javilinos <javilinos@pop-os.localdomain>
2022-12-05 11:34:30 +0100

    new theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,9 @@
+import os
+import sys
+import re
+sys.path.insert(0, os.path.abspath('.'))
+from sphinx.locale import _
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -19,6 +25,7 @@
 
 project = 'Javier Melero Deza'
 copyright = '2022, Javier Melero Deza'
+slug = re.sub(r'\W+', '-', project.lower())
 author = 'Javier Melero Deza'
 
 # The full version, including alpha/beta/rc tags
@@ -31,8 +38,13 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -47,9 +59,77 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    html_theme = 'alabaster'
+    # This is required for the alabaster theme
+    # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
+    html_sidebars = {
+        '**': [
+            'relations.html',  # needs 'show_related': True theme option to display
+            'searchbox.html',
+            ]
+        }
+    sys.stderr.write('Warning: sphinx_rtd_theme missing. Use pip to install it.\n')
+else:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'canonical_url': '',
+        'analytics_id': '',
+        'logo_only': False,
+        'display_version': True,
+        'prev_next_buttons_location': 'None',
+        'style_nav_header_background': 'orange',
+        # Toc options
+        'collapse_navigation': False,
+        'sticky_navigation': True,
+        'navigation_depth': 4,
+    }
+
+html_theme_path = ['_themes']
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_documents = [
+  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
+]
+
+man_pages = [
+    ('index', slug, project, [author], 1)
+]
+
+texinfo_documents = [
+  ('index', slug, project, author, slug, project, 'Miscellaneous'),
+]
+
+def setup(app):
+    from sphinx.domains.python import PyField
+    from sphinx.util.docfields import Field
+
+    app.add_object_type(
+        'confval',
+        'confval',
+        objname='configuration value',
+        indextemplate='pair: %s; configuration value',
+        doc_field_types=[
+            PyField(
+                'type',
+                label=_('Type'),
+                has_arg=False,
+                names=('type',),
+                bodyrolename='class'
+            ),
+            Field(
+                'default',
+                label=_('Default'),
+                has_arg=False,
+                names=('default',),
+            ),
+        ]
+    )
