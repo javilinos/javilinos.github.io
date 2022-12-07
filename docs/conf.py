d05cdb86797f1f2d0fee3af3edd6eaceb858640e
Javilinos <javilinos@pop-os.localdomain>
2022-12-07 17:16:48 +0100

    test

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_mock_imports = ['as2_msgs', 'geographic_msgs']
 
 # -- Options for HTML output -------------------------------------------------
 
